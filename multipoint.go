49e6740198fb2f0e7bad2e617a4867e1ba072d7b
Simon Li <15828908520@163.com>
2018-02-15 16:16:27 +0800

    修改multiPoint.go的描述

--- multipoint.go
+++ multipoint.go
@@ -1,16 +1,16 @@
 package geom
 
-// A MultiPoint is a collection of Points.
+// MultiPoint 是点的集合
 type MultiPoint struct {
 	geom1
 }
 
-// NewMultiPoint returns a new, empty, MultiPoint.
+// NewMultiPoint 函数返回一个新的、空的 MultiPoint
 func NewMultiPoint(layout Layout) *MultiPoint {
 	return NewMultiPointFlat(layout, nil)
 }
 
-// NewMultiPointFlat returns a new MultiPoint with the given flat coordinates.
+// NewMultiPointFlat函数 创建一个新的，有坐标的MultiPoint
 func NewMultiPointFlat(layout Layout, flatCoords []float64) *MultiPoint {
 	mp := new(MultiPoint)
 	mp.layout = layout
@@ -18,34 +18,39 @@ func NewMultiPointFlat(layout Layout, flatCoords []float64) *MultiPoint {
 	mp.flatCoords = flatCoords
 	return mp
 }
+/**
+*------------------------------
+*				MultiPoint（多点）相关的方法
+*---------------------------------
+*/
 
-// Area returns zero.
+// Area方法 返回0
 func (mp *MultiPoint) Area() float64 {
 	return 0
 }
 
-// Clone returns a deep copy.
+// Clone 一个深层拷贝的点
 func (mp *MultiPoint) Clone() *MultiPoint {
 	return deriveCloneMultiPoint(mp)
 }
 
-// Empty returns true if the collection is empty.
+// Empty方法 在点集为空时，返回true
 func (mp *MultiPoint) Empty() bool {
 	return mp.NumPoints() == 0
 }
 
-// Length returns zero.
+// Length方法 返回0
 func (mp *MultiPoint) Length() float64 {
 	return 0
 }
 
-// MustSetCoords sets the coordinates and panics on any error.
+// MustSetCoords方法 设置点集的坐标，当有任何错误时均会抛出
 func (mp *MultiPoint) MustSetCoords(coords []Coord) *MultiPoint {
 	Must(mp.SetCoords(coords))
 	return mp
 }
 
-// SetCoords sets the coordinates.
+// SetCoords方法 设置点集坐标
 func (mp *MultiPoint) SetCoords(coords []Coord) (*MultiPoint, error) {
 	if err := mp.setCoords(coords); err != nil {
 		return nil, err
@@ -53,23 +58,23 @@ func (mp *MultiPoint) SetCoords(coords []Coord) (*MultiPoint, error) {
 	return mp, nil
 }
 
-// SetSRID sets the SRID of mp.
+// SetSRID方法 设置点集的坐标系参考
 func (mp *MultiPoint) SetSRID(srid int) *MultiPoint {
 	mp.srid = srid
 	return mp
 }
 
-// NumPoints returns the number of Points.
+// NumPoints方法 返回点集中点的数目
 func (mp *MultiPoint) NumPoints() int {
 	return mp.NumCoords()
 }
 
-// Point returns the ith Point.
+// Point方法 返回点击中指定索引的点
 func (mp *MultiPoint) Point(i int) *Point {
 	return NewPointFlat(mp.layout, mp.Coord(i))
 }
 
-// Push appends a point.
+// Push方法 向点集中添加新的点
 func (mp *MultiPoint) Push(p *Point) error {
 	if p.layout != mp.layout {
 		return ErrLayoutMismatch{Got: p.layout, Want: mp.layout}
@@ -78,7 +83,7 @@ func (mp *MultiPoint) Push(p *Point) error {
 	return nil
 }
 
-// Swap swaps the values of mp and mp2.
+// Swap方法 将本点集与传入的点集互相交换
 func (mp *MultiPoint) Swap(mp2 *MultiPoint) {
 	*mp, *mp2 = *mp2, *mp
 }
