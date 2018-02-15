2fe35a6941e6e54b5bf1dc71051cb981df151e72
Simon Li <15828908520@163.com>
2018-02-15 16:28:46 +0800

    修改multiLinestring.go的描述

--- multilinestring.go
+++ multilinestring.go
@@ -1,16 +1,16 @@
 package geom
 
-// A MultiLineString is a collection of LineStrings.
+//MultiLineString 是 LineStrings的集合.
 type MultiLineString struct {
 	geom2
 }
 
-// NewMultiLineString returns a new MultiLineString with no LineStrings.
+// NewMultiLineString函数返回一个没有 Linestring 的MultiLinestring
 func NewMultiLineString(layout Layout) *MultiLineString {
 	return NewMultiLineStringFlat(layout, nil, nil)
 }
 
-// NewMultiLineStringFlat returns a new MultiLineString with the given flat coordinates.
+// NewMultiLineStringFlat函数 返回一个新的、有Linestring的MultiLinestring.
 func NewMultiLineStringFlat(layout Layout, flatCoords []float64, ends []int) *MultiLineString {
 	mls := new(MultiLineString)
 	mls.layout = layout
@@ -20,27 +20,33 @@ func NewMultiLineStringFlat(layout Layout, flatCoords []float64, ends []int) *Mu
 	return mls
 }
 
-// Area returns 0.
+/**
+*------------------------------
+*				MultiPoint（多点）相关的方法
+*---------------------------------
+*/
+
+// Area方法 返回0
 func (mls *MultiLineString) Area() float64 {
 	return 0
 }
 
-// Clone returns a deep copy.
+// Clone方法 对MultiPoint进行深层拷贝
 func (mls *MultiLineString) Clone() *MultiLineString {
 	return deriveCloneMultiLineString(mls)
 }
 
-// Empty returns true if the collection is empty.
+// Empty方法 如果此集合为空则返回true
 func (mls *MultiLineString) Empty() bool {
 	return mls.NumLineStrings() == 0
 }
 
-// Length returns the sum of the length of the LineStrings.
+// Length方法 返回集合中 Linestring 长度的和
 func (mls *MultiLineString) Length() float64 {
 	return length2(mls.flatCoords, 0, mls.ends, mls.stride)
 }
 
-// LineString returns the ith LineString.
+// LineString方法 返回指定索引的 Lienstring
 func (mls *MultiLineString) LineString(i int) *LineString {
 	offset := 0
 	if i > 0 {
@@ -49,18 +55,18 @@ func (mls *MultiLineString) LineString(i int) *LineString {
 	return NewLineStringFlat(mls.layout, mls.flatCoords[offset:mls.ends[i]])
 }
 
-// MustSetCoords sets the coordinates and panics on any error.
+// MustSetCoords方法 设置坐标，如果有错将会抛出
 func (mls *MultiLineString) MustSetCoords(coords [][]Coord) *MultiLineString {
 	Must(mls.SetCoords(coords))
 	return mls
 }
 
-// NumLineStrings returns the number of LineStrings.
+// NumLineStrings方法 返回集合中Lienstring的个数
 func (mls *MultiLineString) NumLineStrings() int {
 	return len(mls.ends)
 }
 
-// Push appends a LineString.
+// Push方法 向集合中添加一个Linestring
 func (mls *MultiLineString) Push(ls *LineString) error {
 	if ls.layout != mls.layout {
 		return ErrLayoutMismatch{Got: ls.layout, Want: mls.layout}
@@ -70,7 +76,7 @@ func (mls *MultiLineString) Push(ls *LineString) error {
 	return nil
 }
 
-// SetCoords sets the coordinates.
+// SetCoords方法 设置坐标
 func (mls *MultiLineString) SetCoords(coords [][]Coord) (*MultiLineString, error) {
 	if err := mls.setCoords(coords); err != nil {
 		return nil, err
@@ -78,13 +84,13 @@ func (mls *MultiLineString) SetCoords(coords [][]Coord) (*MultiLineString, error
 	return mls, nil
 }
 
-// SetSRID sets the SRID of mls.
+// SetSRID方法 设置MultiLinestring的坐标系参考
 func (mls *MultiLineString) SetSRID(srid int) *MultiLineString {
 	mls.srid = srid
 	return mls
 }
 
-// Swap swaps the values of mls and mls2.
+// Swap方法 将本集合与传入的集合互相交换
 func (mls *MultiLineString) Swap(mls2 *MultiLineString) {
 	*mls, *mls2 = *mls2, *mls
 }
