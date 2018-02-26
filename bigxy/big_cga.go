599d00dcea9e1d4ba92b11a1585baf653433615d
Simon Li <15828908520@163.com>
2018-02-26 17:00:08 +0800

    完成bigxy的描述

--- bigxy/big_cga.go
+++ bigxy/big_cga.go
@@ -1,8 +1,8 @@
-// Package bigxy contains robust geographic functions on planar (xy) data.  The calculations are performed using
-// big.Float objects for maximum accuracy and robustness.
+// Package bigxy 包含平面（XY）数据的强大地理功能。
+//计算是使用大浮点对象实现的，具有最高的精确度和健壮性。
 //
-// Note: it is required that all coordinates have the x and y ordinates in the 0 and 1 indexed locations in the geom.Coord
-// array.  Given that the coords can be of any size, all data other than x and y is ignored in these calculations.
+// Note:要求所有坐标都必须有x和y坐标，在geom.Coord数组的第一、二位置上。
+// 鉴于坐标可以是任何大小，除了X和Y是在这些计算中忽略了所有的数据。
 package bigxy
 
 import (
@@ -13,23 +13,21 @@ import (
 	"github.com/chengxiaoer/go-geom/xy/orientation"
 )
 
-// dpSafeEpsilon is the value which is safely greater than the
-// relative round-off error in big.Float precision numbers
+// dpSafeEpsilon 该值是安全的，比big.Flaot的相对误差的最大精度数大。
 var dpSafeEpsilon = 1e-15
 
-// OrientationIndex returns the index of the direction of the point point relative to
-// a vector specified by vectorOrigin-vectorEnd
+// OrientationIndex函数 返回一个点的主要方向，相对于由 vectorOrigin-vectorEnd 组成的向量
 //
-// vectorOrigin - the origin point of the vector
-// vectorEnd - the final point of the vector
-// point - the point to compute the direction to
+// vectorOrigin - 矢量的起点
+// vectorEnd - 矢量的终点
+// point - 点计算方向
 //
-// Returns CounterClockwise if point is counter-clockwise (left) from vectorOrigin-vectorEnd
-// Returns Clockwise if point is clockwise (right) from vectorOrigin-vectorEnd
-// Returns Collinear if point is collinear with vectorOrigin-vectorEnd
+// Returns CounterClockwise 如果这点相对于向量是逆时针旋转的
+// Returns Clockwise 如果这个点相对于向量是顺时针旋转的
+// Returns Collinear 如果该点与向量共线
 func OrientationIndex(vectorOrigin, vectorEnd, point geom.Coord) orientation.Type {
-	// fast filter for orientation index
-	// avoids use of slow extended-precision arithmetic in many cases
+	// 定向指标快速滤波器
+	// 避免在许多情况下使用慢扩展精度运算。
 	index := orientationIndexFilter(vectorOrigin, vectorEnd, point)
 	if index <= 1 {
 		return index
@@ -37,13 +35,13 @@ func OrientationIndex(vectorOrigin, vectorEnd, point geom.Coord) orientation.Typ
 
 	var dx1, dy1, dx2, dy2 big.Float
 
-	// normalize coordinates
+	// 归一化坐标
 	dx1.SetFloat64(vectorEnd[0]).Add(&dx1, big.NewFloat(-vectorOrigin[0]))
 	dy1.SetFloat64(vectorEnd[1]).Add(&dy1, big.NewFloat(-vectorOrigin[1]))
 	dx2.SetFloat64(point[0]).Add(&dx2, big.NewFloat(-vectorEnd[0]))
 	dy2.SetFloat64(point[1]).Add(&dy2, big.NewFloat(-vectorEnd[1]))
 
-	// calculate determinant.  Calculation takes place in dx1 for performance
+	// 计算因子.  计算的性能主要体现在 dx1 上
 	dx1.Mul(&dx1, &dy2)
 	dy1.Mul(&dy1, &dx2)
 	dx1.Sub(&dx1, &dy1)
@@ -51,9 +49,9 @@ func OrientationIndex(vectorOrigin, vectorEnd, point geom.Coord) orientation.Typ
 	return orientationBasedOnSignForBig(dx1)
 }
 
-// Intersection computes the intersection point of the two lines using math.big.Float arithmetic.
-// The lines are considered infinate in length.  For example, (0,0), (1, 0) and (2, 1) (2, 2) will have intersection of (2, 0)
-// Currently does not handle case of parallel lines.
+// Intersection函数 使用数学计算两条直线的交点。大浮点运算.
+// Line被认为是无限长的线（可以双向延展）。  For example, (0,0), (1, 0) and (2, 1) (2, 2) 将相较于 (2, 0)
+// 目前未处理平行线的情况。
 func Intersection(line1Start, line1End, line2Start, line2End geom.Coord) geom.Coord {
 	var denom1, denom2, denom, tmp1, tmp2 big.Float
 
@@ -62,11 +60,11 @@ func Intersection(line1Start, line1End, line2Start, line2End geom.Coord) geom.Co
 	denom.Sub(&denom1, &denom2)
 
 	// Cases:
-	// - denom is 0 if lines are parallel
-	// - intersection point lies within line segment p if fracP is between 0 and 1
-	// - intersection point lies within line segment q if fracQ is between 0 and 1
+	// - denom is 0 如果直线相互平行
+	// - 如果 fracp的值在0和1之间，交点位于线段的p上
+	// - 如果fracQ的值在0和1之间，交点位于线段Q上
 
-	// reusing previous variables for performance
+	// 重用以前的变量以获得性能
 	numx1 := &denom1
 	numx2 := &denom2
 	var numx big.Float
@@ -79,7 +77,7 @@ func Intersection(line1Start, line1End, line2Start, line2End geom.Coord) geom.Co
 
 	x, _ := numx1.SetFloat64(line1Start[0]).Add(numx1, tmp2.SetFloat64(line1End[0])).Sub(numx1, tmp2.SetFloat64(line1Start[0])).Mul(numx1, tmp1.SetFloat64(fracP)).Float64()
 
-	// reusing previous variables for performance
+	// 重用以前的变量以获得性能
 	numy1 := &denom1
 	numy2 := &denom2
 	var numy big.Float
@@ -102,23 +100,19 @@ func Intersection(line1Start, line1End, line2Start, line2End geom.Coord) geom.Co
 	return geom.Coord{x, y}
 }
 
-/////////////////  Implementation /////////////////////////////////
+/////////////////  实现 /////////////////////////////////
 
-// A filter for computing the orientation index of three coordinates.
+// 一种计算三坐标方位指数的过滤器。
 //
-// If the orientation can be computed safely using standard DP
-// arithmetic, this routine returns the orientation index.
-// Otherwise, a value i > 1 is returned.
-// In this case the orientation index must
-// be computed using some other more robust method.
-// The filter is fast to compute, so can be used to
-// avoid the use of slower robust methods except when they are really needed,
-// thus providing better average performance.
+// 如果可以，使用标准DP算法安全地计算方向, 这个goroutine返回方向索引。
+// 其他情况, 一个值 i > 1 将被返回
+// 在这种情况下，方向指数必须用其他更可靠的方法计算。
+// 该过滤器是快速计算，所以可以用来避免使用较慢的强健方法，除非他们真的需要，从而提供更好的平均性能。
 //
-// Uses an approach due to Jonathan Shewchuk, which is in the public domain.
-//
-// Return the orientation index if it can be computed safely
-// Return i > 1 if the orientation index cannot be computed safely
+// Uses an approach due to Jonathan Shewchuk, which is in the public domain（这是一个公共领域）.
+// Jonathan Shewchuk---分治法中三角形的几何信息和拓扑信息的操作
+// Return 方向索引，如果它被安全的计算
+// Return i > 1，如果这个这个方向索引不是安全计算的
 func orientationIndexFilter(vectorOrigin, vectorEnd, point geom.Coord) orientation.Type {
 	var detsum float64
 
